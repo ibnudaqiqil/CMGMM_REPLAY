f9bb581129d3e718789a345e6dc8aa189609ad7e
Ibnu Daqiqil Id <ibnu.daqiqil@lecturer.unri.ac.id>
2022-01-10 00:52:01 +0900

    Update CGAN.py

--- models/CGAN.py
+++ models/CGAN.py
@@ -74,8 +74,7 @@ class Discriminator(nn.Module):
 
     def forward(self, img, labels):
         # Concatenate label embedding and image to produce input
-        d_in = torch.cat((img.view(img.size(0), -1),
-                          self.label_embedding(labels)), -1)
+        d_in = torch.cat((img.view(img.size(0), -1), self.label_embedding(labels)), -1)
         validity = self.model(d_in)
         return validity
 
@@ -90,7 +89,13 @@ def train_replayer(dataloader, n_classes,writer):
     batch_size = 64
     sample_interval= 10
     latent_dim = 100
-    img_shape=28
+    channels=1
+    img_size =28
+    img_shape =  (channels, img_size, img_size)
+
+    cuda = True if torch.cuda.is_available() else False
+    FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+    LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
     # Loss function
     adversarial_loss = torch.nn.MSELoss() #torch.nn.BCELoss()
 
@@ -115,47 +120,24 @@ def train_replayer(dataloader, n_classes,writer):
         discriminator.parameters(), lr=learning_rate, betas=(beta1,beta2))
 
 
-    batches_done = 0
+    # ----------
+    #  Training
+    # ----------
+    batches_done=0
     for epoch in range(n_epochs):
         for i, (imgs, labels) in enumerate(dataloader):
-            Batch_Size = batch_size
-            N_Class = n_classes
+
+            batch_size = imgs.shape[0]
+
             # Adversarial ground truths
-            valid = Variable(torch.ones(Batch_Size).cuda(), requires_grad=False)
-            fake = Variable(torch.zeros(Batch_Size).cuda(), requires_grad=False)
+            valid = Variable(FloatTensor(batch_size, 1).fill_(
+                1.0), requires_grad=False)
+            fake = Variable(FloatTensor(batch_size, 1).fill_(0.0),
+                            requires_grad=False)
 
             # Configure input
-            real_imgs = Variable(imgs.type(torch.FloatTensor).cuda())
-
-            real_y = torch.zeros(Batch_Size, N_Class)
-            real_y = Variable(real_y.scatter_(
-                1, labels.view(Batch_Size, 1), 1).cuda())
-            #y = Variable(y.cuda())
-
-            # Sample noise and labels as generator input
-            noise = Variable(torch.randn((Batch_Size, latent_dim)).cuda())
-            gen_labels = (torch.rand(Batch_Size, 1) *
-                        N_Class).type(torch.LongTensor)
-            gen_y = torch.zeros(Batch_Size, N_Class)
-            gen_y = Variable(gen_y.scatter_(
-                1, gen_labels.view(Batch_Size, 1), 1).cuda())
-
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
-            optimizer_D.zero_grad()
-            # Loss for real images
-            d_real_loss = adversarial_loss(
-                discriminator(real_imgs, real_y).squeeze(), valid)
-            # Loss for fake images
-            gen_imgs = generator(noise, gen_y)
-            d_fake_loss = adversarial_loss(discriminator(
-                gen_imgs.detach(), gen_y).squeeze(), fake)
-            # Total discriminator loss
-            d_loss = (d_real_loss + d_fake_loss)
-
-            d_loss.backward()
-            optimizer_D.step()
+            real_imgs = Variable(imgs.type(FloatTensor))
+            labels = Variable(labels.type(LongTensor))
 
             # -----------------
             #  Train Generator
@@ -163,29 +145,49 @@ def train_replayer(dataloader, n_classes,writer):
 
             optimizer_G.zero_grad()
 
+            # Sample noise and labels as generator input
+            z = Variable(FloatTensor(np.random.normal(
+                0, 1, (batch_size, latent_dim))))
+            gen_labels = Variable(LongTensor(
+                np.random.randint(0, n_classes, batch_size)))
+
             # Generate a batch of images
-            #gen_imgs = generator(noise, gen_y)
+            gen_imgs = generator(z, gen_labels)
+
             # Loss measures generator's ability to fool the discriminator
-            g_loss = adversarial_loss(discriminator(
-                gen_imgs, gen_y).squeeze(), valid)
+            validity = discriminator(gen_imgs, gen_labels)
+            g_loss = adversarial_loss(validity, valid)
 
             g_loss.backward()
             optimizer_G.step()
 
-            print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, n_epochs, i, len(dataloader),
-                                                                            d_loss.data.cpu(), g_loss.data.cpu()))
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            optimizer_D.zero_grad()
+
+            # Loss for real images
+            validity_real = discriminator(real_imgs, labels)
+            d_real_loss = adversarial_loss(validity_real, valid)
+
+            # Loss for fake images
+            validity_fake = discriminator(gen_imgs.detach(), gen_labels)
+            d_fake_loss = adversarial_loss(validity_fake, fake)
+
+            # Total discriminator loss
+            d_loss = (d_real_loss + d_fake_loss) / 2
+
+            d_loss.backward()
+            optimizer_D.step()
+
+            print(
+                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+            )
 
             batches_done = epoch * len(dataloader) + i
-            if batches_done % sample_interval == 0:
-                noise = Variable(torch.FloatTensor(np.random.normal(
-                    0, 1, (N_Class**2, latent_dim))).cuda())
-                #fixed labels
-                y_ = torch.LongTensor(np.array([num for num in range(N_Class)])).view(N_Class, 1).expand(-1, N_Class).contiguous()
-                y_fixed = torch.zeros(N_Class**2, N_Class)
-                y_fixed = Variable(y_fixed.scatter_( 1, y_.view(N_Class**2, 1), 1).cuda())
-
-                #gen_imgs = generator(noise, y_fixed).view(-1, C, H, W)
-                gen_imgs = generator(noise, y_fixed).view(-1, 1, 28, 28)
+            
 
                 #save_image(gen_imgs.data, img_save_path + '/%d-%d.png' % (epoch, batches_done), nrow=N_Class, normalize=True)
     return generator, discriminator
